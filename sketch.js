17316c4e4415f312e8f231746ddb18dc506c6720
leon0212 <leonhui0212@gmail.com>
2016-07-25 12:25:07 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,4 +1,6 @@
 var genbutton;
+var genbutton2;
+var genbutton3;
 var genslider;
 var running = true;
 
@@ -13,6 +15,16 @@ function setup() {
   genbutton.mouseClicked(turnonoff);
   genbutton.position((width/2)-50,(height/2)-90);
   genbutton.class("sim-button");
+  
+  genbutton2 = createButton("STOP");
+  genbutton2.mouseClicked(turnonoff);
+  genbutton2.position(300,30);
+  genbutton2.class("sim-button.gray");
+  
+  genbutton3 = createButton("STOP");
+  genbutton3.mouseClicked(turnonoff);
+  genbutton3.position(150,30);
+  genbutton3.class("sim-button.red");
   
   genbutton = createButton("STOP");
   genbutton.mouseClicked(turnonoff);
@@ -29,7 +41,7 @@ function setup() {
 function draw() {
   background(255);
   fill(230,40,40);
- ellipse((width/2)+random(0,5),((height/2) +300)+random(0,5),genslider.value(),genslider.value());
+ ellipse((width/2)+random(0,5),((height/2) +200)+random(0,5),genslider.value(),genslider.value());
 }
 
 function turnonoff() {
@@ -48,4 +60,3 @@ function turnonoff() {
       return
     }
 }
-
